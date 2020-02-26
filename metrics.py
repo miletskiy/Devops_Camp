dc52adaa691f37928094296900927e2baabacdef
Miletskyi Sergii <smiletskyi@intellias.com>
2020-02-26 10:20:15 +0200

    Add args parsing

--- metrics.py
+++ metrics.py
@@ -1,27 +1,39 @@
 # metrics.py
 
 import psutil
+import argparse
+
+parser = argparse.ArgumentParser(description = 'Metrics collection script')
+msg = 'Please use `cpu` for CPU metrics or `mem` for memory metrics'
+parser.add_argument('type', type=str,
+    help=f'Type of metric. {msg}')
+args = parser.parse_args()
 
 
-cpu_result = psutil.cpu_times_percent()
+if args.type == 'cpu':
 
-print(f'system.cpu.idle {cpu_result.idle}')
-print(f'system.cpu.user {cpu_result.user}')
-print(f'system.cpu.guest ')   # TODO
-print(f'system.cpu.iowait ')  # TODO
-print(f'system.cpu.stolen ')  # TODO
-print(f'system.cpu.system {cpu_result.system}')
+    cpu_result = psutil.cpu_times_percent()
 
+    print(f'system.cpu.idle {cpu_result.idle}')
+    print(f'system.cpu.user {cpu_result.user}')
+    print(f'system.cpu.guest ')   # TODO
+    print(f'system.cpu.iowait ')  # TODO
+    print(f'system.cpu.stolen ')  # TODO
+    print(f'system.cpu.system {cpu_result.system}')
 
-mem_result = psutil.virtual_memory()
+elif args.type == 'mem':
+    mem_result = psutil.virtual_memory()
 
-print(f'virtual total {mem_result.total}')
-print(f'virtual used {mem_result.used}')
-print(f'virtual free {mem_result.free}')
-print(f'virtual shared ') # TODO
+    print(f'virtual total {mem_result.total}')
+    print(f'virtual used {mem_result.used}')
+    print(f'virtual free {mem_result.free}')
+    print(f'virtual shared ') # TODO
 
-swap_result = psutil.swap_memory()
+    swap_result = psutil.swap_memory()
 
-print(f'swap total {swap_result.total}')
-print(f'swap used {swap_result.used}')
-print(f'swap free {swap_result.free}')
+    print(f'swap total {swap_result.total}')
+    print(f'swap used {swap_result.used}')
+    print(f'swap free {swap_result.free}')
+
+else:
+    print(msg)
